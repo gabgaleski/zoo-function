50dc337708ebba83d012af72a17207271c93dbd8
gabgaleski <galeski35@gmail.com>
2023-01-25 19:38:35 -0300

    Realiza requisito 1 e 2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (id1, id2) => {
+  const animals = [];
+  data.species.forEach((animal) => {
+    if (animal.id === id1 || animal.id === id2) {
+      animals.push(animal);
+    }
+  });
+  return animals;
 };
 
 module.exports = getSpeciesByIds;
