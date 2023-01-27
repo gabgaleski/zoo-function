484edd259ad8d0f32d0da507762b8fb19d7b9868
gabgaleski <galeski35@gmail.com>
2023-01-27 15:04:48 -0300

    Realiza requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    const obj = {};
+    data.species.forEach((element) => {
+      obj[element.name] = element.residents.length;
+    });
+    return obj;
+  }
+  const animals = data.species.find((objAnimal) => objAnimal.name === animal.species);
+  const animalsSex = animals.residents.filter((element) => element.sex === animal.sex);
+  if (animal.sex === undefined) {
+    return animals.residents.length;
+  }
+  return animalsSex.length;
 };
-
 module.exports = countAnimals;
