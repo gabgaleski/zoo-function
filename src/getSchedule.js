88be2cee3a827a30e75d42dc7ad3b9f353a9094b
gabgaleski <galeski35@gmail.com>
2023-01-30 18:16:07 -0300

    Realiza requisito 10

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+    const obj = data.hours;
+    const objKeys = Object.keys(obj);
+    const objValues = Object.values(obj);
+    const objOpen = objValues.map((element) => element.open);
+    const objClose = objValues.map((element) => element.close);
+    const filter = data.species.filter((animal) => animal.availability);
+    return objKeys.map((element, index) => ({
+      [objKeys[index]]: {
+        officeHour: `Open from ${objOpen[index]}am until ${objClose[index]}pm`,
+        exhibition: 
+      },
+    }));
+  }
+  const getAnimal = data.species.find((animal) => scheduleTarget === animal.name);
+  return getAnimal.availability;
 };
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
